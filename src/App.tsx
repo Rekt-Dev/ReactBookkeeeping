b647196e92ef50b6aca23081796fd414c97ee937
Rekt-Dev <89317064+Rekt-Dev@users.noreply.github.com>
2022-05-29 00:03:16 +0300

    toying with routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./styles.css";
 import { BrowserRouter, Link } from "react-router-dom";
+import Expenses from "./routes/expenses";
 
 export default function App() {
   return (
@@ -7,14 +8,15 @@ export default function App() {
       <h1>React Bookeeping App</h1>
       <nav
         style={{
-          borderBottom: "solid 1px",
+          borderBottom: "solid 3px",
           paddingBottom: "1rem"
         }}
       >
         <Link to="/invoices">Invoices</Link> |{" "}
-        <Link to="/expenses">Expenses</Link> | <Link to="/cash">Cashflow</Link>{" "}
-        | <Link to="/debt">Debt</Link>
+        <Link to="/routes/expenses.tsx">Expenses</Link> |{" "}
+        <Link to="/cash">Cashflow</Link> | <Link to="/debt">Debt</Link>
       </nav>
+      <Expenses />
     </div>
   );
 }
